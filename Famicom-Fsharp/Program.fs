0dba87d229b6d26b8caaea3957f29169bbf7b4c6
Kunihiro Ando <senna5150ando@gmail.com>
2022-05-02 16:09:47 +0900

    fps

--- Famicom-Fsharp/Program.fs
+++ Famicom-Fsharp/Program.fs
@@ -6,7 +6,9 @@ open NES
 let asUint32 (r, g, b) = BitConverter.ToUInt32 (ReadOnlySpan [|b; g; r; 255uy|])
 [<EntryPoint>]
 let main argv =
-    let ticktime = 0
+    let mutable lastTick = 0
+    let mutable frameRate = 0
+
     let nes = new Nes("sm.nes")
     nes.initNes
     let (width, height) = 256, 240
@@ -27,17 +29,25 @@ let main argv =
         if imgflg then
             let rgbary = nes.get_img_data
             nes.clearImg
-
-            let mutable i = 0
-            while i < 256*240-1 do
-                frameBuffer[i] <- (asUint32(rgbary[i], rgbary[i+1],rgbary[i+2]))
-                i <- i + 2
-
+            // let mutable i = 0
+            // while i < 256*240-1 do
+            //     frameBuffer[i] <- (asUint32(rgbary[i], rgbary[i+1],rgbary[i+2]))
+            //     i <- i + 2
             SDL_UpdateTexture(texture, IntPtr.Zero, bufferPtr, width * 4) |> ignore
             SDL_RenderClear(renderer) |> ignore
             SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero) |> ignore
             SDL_RenderPresent(renderer) |> ignore
-            
+            frameRate<- frameRate+1
+
+        if (System.Environment.TickCount - lastTick) >= 1000 then
+            printfn "%d" frameRate
+            frameRate <- 0
+            lastTick <- System.Environment.TickCount
+
+
+
+
+
         if (keyEvent.``type`` = SDL_QUIT) then
             ()
         else if SDL_PollEvent(&keyEvent) = 0 || (keyEvent.``type`` <> SDL_KEYDOWN && keyEvent.``type`` <> SDL_KEYUP) then
